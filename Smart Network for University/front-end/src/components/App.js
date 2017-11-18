8bb9b6b48ae7ee47ebb1813c4ecf7c56c9e15d1f
panchaltej <tejaspanchal49@gmail.com>
2017-11-18 02:24:51 -0800

    Adding homepage UI in app.js

--- Smart Network for University/front-end/src/components/App.js
+++ Smart Network for University/front-end/src/components/App.js
@@ -1,17 +1,78 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 
 class App extends Component {
 
-  render() {
-    return (
-      <div className = "row">
-          Test
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+                    <button
+                        className="navbar-toggler navbar-toggler-right hidden-lg-up"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarsExampleDefault"
+                        aria-controls="navbarsExampleDefault"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">Dashboard</a>
 
+                    <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Home
+                                    <span className="sr-only">(current)</span>
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Settings</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Profile</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Help</a>
+                            </li>
+                        </ul>
+                        <form className="form-inline mt-2 mt-md-0">
+                            <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        <nav className="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
+                            <ul className="nav nav-pills flex-column">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="#">Home
+                                        <span className="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Messages</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Files</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Requests</a>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
+                            
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
