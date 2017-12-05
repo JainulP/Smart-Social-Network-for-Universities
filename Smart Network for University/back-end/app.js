f0cf9b72782a4691711fc93b1b59356677725166
anudeepadinesh <anudeepadinesh@gmail.com>
2017-12-05 14:39:25 -0800

    msg

--- Smart Network for University/back-end/app.js
+++ Smart Network for University/back-end/app.js
@@ -28,6 +28,8 @@ var uploadFiles = require('./routes/uploadFiles');
 var communities = require('./routes/communities');
 var announcement = require('./routes/announcement');
 
+// require('dotenv').config();
+
 
 
 var app = express();
@@ -92,5 +94,46 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json('error');
 });
+
+
+//
+// // load aws sdk
+// var aws = require('aws-sdk');
+//
+// // load aws config
+// aws.config.loadFromPath('config.json');
+//
+// // load AWS SES
+// var ses = new aws.SES();
+//
+// // send to list
+// var to = ['jainul.patel11@gmail.com','tejas.panchal@sjsu.edu']
+//
+// // this must relate to a verified SES account
+// var from = 'jainul.patel@sjsu.edu'
+//
+//
+// // this sends the email
+// // @todo - add HTML version
+// ses.sendEmail( {
+//     Source: from,
+//     Destination: { ToAddresses: to },
+//     Message: {
+//         Subject: {
+//     Data: 'Demo Announcement'
+// },
+// Body: {
+//     Text: {
+//         Data: 'Demo Announcement',
+//     }
+// }
+// }
+// }
+// , function(err, data) {
+//     if(err) throw err
+//     console.log('Email sent:');
+//     console.log(data);
+// });
+
 
 module.exports = app;
